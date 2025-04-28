13ec1795442195f13fc3365f62a16fdf315e1a1d
NR <nicholas.rebello@gmail.com>
2025-04-28 16:38:48 -0400

    title screen

--- main.py
+++ main.py
@@ -1,37 +1,67 @@
 import pygame
+import sys
+import random
 
+from data.classes.TitleScreen import TitleScreen
 from data.classes.Board import Board
+# Other imports like Piece, Square, etc.
 
+# Initialize pygame
 pygame.init()
 
-WINDOW_SIZE = (600, 600)
-screen = pygame.display.set_mode(WINDOW_SIZE)
+WIDTH, HEIGHT = 800, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Chess')
 
-board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1])
+FPS = 60
+CLOCK = pygame.time.Clock()
 
-def draw(display):
-	display.fill('white')
-	board.draw(display)
-	pygame.display.update()
+# Game variables
+board = Board(WIDTH, HEIGHT)
 
+def draw(screen):
+    board.draw(screen)
+    pygame.display.update()
+
+def title_loop():
+    title_screen = TitleScreen(screen)
+
+    while True:
+        CLOCK.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            result = title_screen.handle_event(event)
+            if result == "pvp":
+                return "pvp"
+
+        title_screen.draw()
 
 if __name__ == '__main__':
-	running = True
-	while running:
-		mx, my = pygame.mouse.get_pos()
-		for event in pygame.event.get():
-			# Quit the game if the user presses the close button
-			if event.type == pygame.QUIT:
-				running = False
-			elif event.type == pygame.MOUSEBUTTONDOWN: 
-       			# If the mouse is clicked
-				if event.button == 1:
-					board.handle_click(mx, my)
-		if board.is_in_checkmate('black'): # If black is in checkmate
-			print('White wins!')
-			running = False
-		elif board.is_in_checkmate('white'): # If white is in checkmate
-			print('Black wins!')
-			running = False
-		# Draw the board
-		draw(screen)
+    mode = title_loop()  # Go to title screen first
+    if mode == "pvp":
+        running = True
+        while running:
+            CLOCK.tick(FPS)
+            mx, my = pygame.mouse.get_pos()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        board.handle_click(mx, my)
+
+            if board.is_in_checkmate('black'):
+                print('White wins!')
+                running = False
+            elif board.is_in_checkmate('white'):
+                print('Black wins!')
+                running = False
+
+            draw(screen)
+
+    pygame.quit()
+    sys.exit()
